746cc4a579cb6c892c76699cbd8e6a55bad5e9c6
interactivestoryteller <132190259+interactivestoryteller@users.noreply.github.com>
2023-04-30 14:37:09 +0530

    Add files via upload

---
+++ format.js
@@ -0,0 +1,7 @@
+window.storyFormat({
+  name: 'My Screenplay Format',
+  version: '1.0.0',
+  author: 'Sumit Saxena',
+  description: 'A custom story format for writing screenplays in Twine 2.0.',
+  url: 'https://yourdomain.com/my-screenplay-format'
+});
